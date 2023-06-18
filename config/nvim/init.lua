d937f362748aff6a2cd208df93556dd0d403c487
Kurt L. Manion <klmanion@gmail.com>
2023-06-18 12:39:25 -0400

    nethackrc

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -92,12 +92,16 @@ require('packer').startup(function(use)
 --	use 'williamboman/nvim-lsp-installer'
 
 	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+	use { 'nvim-orgmode/orgmode', config = function()
+		require('orgmode').setup{}
+	end
+	}
 
-	use 'morhetz/gruvbox'
+--	use 'morhetz/gruvbox'
 
 	use 'vim-syntastic/syntastic'
 --	use { 'neoclide/coc.nvim', branch = 'release' }
-	use 'dense-analysis/ale'
+--	use 'dense-analysis/ale'
 
 	use 'neovimhaskell/haskell-vim'
 	use 'alx741/vim-hindent'
@@ -113,15 +117,23 @@ vim.g.syntastic_auto_loc_list = 1
 vim.g.syntastic_check_on_open = 1
 vim.g.syntastic_check_on_wq = 0
 
+--- orgmode
+require('orgmode').setup_ts_grammar()
+
+require('orgmode').setup({
+	org_agenda_files = {'~/org/*'}
+	-- org_default_notes_file = '~/Dropbox/org/refile.org',
+})
+
 --- ale
 --vim.g.ale_lint_on_enter = 0
 
 --- colorscheme
 vim.g.gruvbox_contrast_dark = [['hard']]
-vim.cmd('colorscheme gruvbox')
+--vim.cmd('colorscheme gruvbox')
 
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = { "c", "haskell", "scheme", "latex", "bash", "lua", "rust" },
+	ensure_installed = { "c", "org", "haskell", "scheme", "latex", "bash", "lua", "rust" },
 
 	highlight = {
 		enable = true,
@@ -130,73 +142,10 @@ require'nvim-treesitter.configs'.setup {
 		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
 		-- Using this option may slow down your editor, and you may see some duplicate highlights.
 		-- Instead of true it can also be a list of languages
-		additional_vim_regex_highlighting = false,
+		additional_vim_regex_highlighting = {'org'},
 	},
 	indent = {
 		enable = true,
 	},
 }
-
---require'nvim-lsp-installer'.setup ({
---	automatic_installation = true,
---})
---
---local saga = require 'lspsaga'
---saga.init_lsp_saga()
---
---local nvim_lsp = require('lspconfig')
---
----- Mappings.
----- See `:help vim.diagnostic.*` for documentation on any of the below functions
---local opts = { noremap=true, silent=true }
---vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
---vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
---vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
---vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
---
----- Use an on_attach function to only map the following keys
----- after the language server attaches to the current buffer
---local on_attach = function(client, bufnr)
---  -- Enable completion triggered by <c-x><c-o>
---  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
---
---  -- Mappings.
---  -- See `:help vim.lsp.*` for documentation on any of the below functions
---  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
---  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
---  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
---  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
---  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
---  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
---  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
---  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
---  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
---  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-----  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
---  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
---  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
---
---  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', ':Lspsaga code_action<CR>', opts)
---  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gh', '<cmd>Lspsaga lsp_finder<CR>', opts)
---end
---
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
---local servers = { 'rust_analyzer', 'hls', 'bashls', 'awk_ls', 'ltex', 'texlab',
---	'racket_langserver', 'sumneko_lua', }
---for _, lsp in pairs(servers) do
---  require('lspconfig')[lsp].setup {
---    on_attach = on_attach,
---  }
---end
---
---require'lspconfig'.sumneko_lua.setup {
---	settings = {
---		Lua = {
---			diagnostics = {
---				globals = { 'vim' }
---			}
---		}
---	}
---}
 
